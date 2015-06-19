ab58c8004a5f463ff29299a86a1641bf1d1043fe
Artiom <a.mv@gmx.fr>
2015-06-19 13:30:15 +0200

    Minor changes for Pyhton3 compatibility

--- python/fotolia_api/__init__.py
+++ python/fotolia_api/__init__.py
@@ -26,6 +26,13 @@ THUMB_SIZE_SMALL = 110
 THUMB_SIZE_MEDIUM = 160
 THUMB_SIZE_LARGE = 400
 
+import os, sys, inspect
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+	sys.path.insert(0, cmd_folder)
+
+
 import rest as _rest
 import error as _error
 
